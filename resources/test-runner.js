18dfc18c6051971a59d6e904d5620d6abe4e4b55
Alexander Schmitz <arschmitz@gmail.com>
2015-03-25 22:24:43 -0400

    Add button to run tests

---
+++ resources/test-runner.js
@@ -0,0 +1,9 @@
+$(function(){
+	var testButton = $( "<button class='test-runner-button'>Run Tests</button>" );
+	$( "body" ).append( testButton );
+	testButton.on( "click", function(){
+		$( "#qunit" ).show();
+		$( this ).hide();
+		runTests();
+	});
+});
